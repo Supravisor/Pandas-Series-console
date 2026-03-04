5ef7692669513b221a12bedbf932f9b6919e6e7b
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-03-04 15:49:26 +1300

    add createFromObject function

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ const series = () => {
      document.editor.textbox.value+= "\n" + variable.value + " = pd.Series([" + array.value.replaceAll(",", ", ").split(",") + "])";
   }
 }
+
+const createFromObject = () => {
+  if (variable.value === "") {
+    return alert("Please enter a variable name in the 'variable' field, in the 'pandas.Series object' section.");
+  } else if (value.value === '') {
+    return alert("Please enter a value in the 'value' field, in the 'pandas.Series object' section.");
+  } else {
+      document.editor.textbox.value+="\n" + variable.value + " = pd.Series({\n    '': ,\n    '': \n}, name='" + value.value + "')";
+  }
+}
