15457f27d6c47bb6a006758a46a3dec71de731a5
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-03-04 16:11:19 +1300

    add createFromList function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let variable = document.getElementById("variable");
 let value = document.getElementById("value");
 
 const series = () => {
-  if (array.value === '') {
+  if (array.value === "") {
       return alert("Please enter a comma separated array in the 'array' field, in the 'pandas.Series object' section.");
   } else if (variable.value === "") {
      document.editor.textbox.value+= "\nnp.array([" + array.value.replaceAll(",", ", ").split(",") + "])";
@@ -19,9 +19,19 @@ const series = () => {
 const createFromObject = () => {
   if (variable.value === "") {
     return alert("Please enter a variable name in the 'variable' field, in the 'pandas.Series object' section.");
-  } else if (value.value === '') {
+  } else if (value.value === "") {
     return alert("Please enter a value in the 'value' field, in the 'pandas.Series object' section.");
   } else {
       document.editor.textbox.value+="\n" + variable.value + " = pd.Series({\n    '': ,\n    '': \n}, name='" + value.value + "')";
+  }
+}
+
+const createFromList = () => {
+  if (variable.value === "") {
+    return alert("Please enter a variable name in the 'variable' field.");
+  } else if (value.value === "") {
+    return alert("Please enter a value in the 'value' field.");
+  } else {
+      document.editor.textbox.value+="\n" + variable.value + " = pd.Series(\n    [ , , ],\n    index=['', ''],\n    name='" + value.value + "')";
   }
 }
