c60e8898942aae60582b0774f0d00f231837cff1
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-03-05 12:02:05 +1300

    add seriesProperties function

--- script.js
+++ script.js
@@ -45,3 +45,11 @@ const createFromSeries = () => {
       document.editor.textbox.value+="\n" + array.value + " = pd.Series(" + variable.value + ", index=['', ''])";
   }
 }
+
+const seriesProperties = (arg) => {
+  if (variable.value === "") {
+    return alert("Please enter a variable name in the 'variable' field, in the 'pandas.Series object' section.");
+  } else {
+      document.editor.textbox.value+="\n" + variable.value + "." + arg;
+  }
+}
