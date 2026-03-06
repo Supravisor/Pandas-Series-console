640726bb27865e668ccc76060aa1b9c259ee3463
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-03-06 16:27:18 +1300

    add assignValue variable

--- script.js
+++ script.js
@@ -129,6 +129,7 @@ const dtype = (type) => {
 
 // Assigning
 let assignIndexValue = document.getElementById("assignIndexValue");
+let assignValue = document.getElementById("assignValue");
 
 const assignSeries = () => {
   if (variable.value === "") {
