ccd2342bde34768ef0435d0d40534fe88256ab64
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-03-11 11:43:49 +1300

    add indexSeriesRangeTwo variable

--- script.js
+++ script.js
@@ -231,6 +231,7 @@ const assignIndex = () => {
 
 // Slicing
 let indexSeriesRangeOne = document.getElementById("indexSeriesRangeOne");
+let indexSeriesRangeTwo = document.getElementById("indexSeriesRangeTwo");
 
 const sliceRange = () => {
   if (variable.value === "") {
