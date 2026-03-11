2a91a59bb4ff7fa8c20f644504a5d5de8d34b38d
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-03-11 13:45:10 +1300

    add rangeMatrixOdd function

--- script.js
+++ script.js
@@ -289,3 +289,13 @@ const rangeMatrixReverse = () => {
       document.editor.textbox.value+= "\n" + variable.value + "[::-1]";
   }
 }
+
+const rangeMatrixOdd = () => {
+  if (variable.value === "") {
+    return alert("Please enter a variable name in the 'variable' field, in the 'pandas.Series object' section.");
+  } else if (Math.abs(Number(variable.value)) >= 0) {
+      return alert("Please do not enter a number in the 'variable' field, in the 'pandas.Series object' section.");
+  } else {
+      document.editor.textbox.value+= "\n" + variable.value + "[::2]";
+  }
+}
